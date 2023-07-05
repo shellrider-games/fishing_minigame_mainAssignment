ef3e39852f404810021125a5348af6f44fd0c92e
Georg Becker <Georg.Becker@gmx.at>
2023-07-05 15:35:59 +0200

    work on fishing pole follow behaviour

--- FishingGame/Assets/Scripts/FollowMouseLook.cs
+++ FishingGame/Assets/Scripts/FollowMouseLook.cs
@@ -9,10 +9,13 @@ public class FollowMouseLook : MonoBehaviour
     [SerializeField] private MouseLookControl followTarget;
     [SerializeField] private float distance;
     [SerializeField] private float followTime;
-    [SerializeField] private Vector3 offset;
 
     private Vector3 _previousPosition;
     private Vector3 _targetPostition;
+
+    private Quaternion _previousRotation;
+    private Quaternion _targetRotation;
+    
     private float _elapsedTime;
     private void Awake()
     {
@@ -21,15 +24,20 @@ public class FollowMouseLook : MonoBehaviour
             followTarget.AfterLookAround += SetSlerpTarget;
         }
         _elapsedTime = followTime;
-        transform.position = followTarget.transform.position + offset + distance * followTarget.transform.forward;
+        transform.position = followTarget.transform.position  + distance * followTarget.transform.forward;
         _targetPostition = transform.position;
         _previousPosition = transform.position;
+        transform.rotation = followTarget.transform.rotation;
+        _targetRotation = transform.rotation;
+        _previousRotation = transform.rotation;
     }
 
     private void SetSlerpTarget(Transform target)
     {
         _previousPosition = transform.position;
-        _targetPostition = target.position + offset + distance * target.forward;
+        _targetPostition = target.position + distance * target.forward;
+        _previousRotation = transform.rotation;
+        _targetRotation = target.rotation;
         _elapsedTime = 0;
     }
     
@@ -39,7 +47,8 @@ public class FollowMouseLook : MonoBehaviour
         if (_elapsedTime < followTime)
         {
             _elapsedTime += Time.deltaTime;
-            transform.position = Vector3.Slerp(_previousPosition, _targetPostition, _elapsedTime);
+            transform.position = Vector3.Slerp(_previousPosition, _targetPostition, _elapsedTime/followTime);
+            transform.rotation = Quaternion.Slerp(_previousRotation, _targetRotation, _elapsedTime / followTime);
         }
     }
 }
